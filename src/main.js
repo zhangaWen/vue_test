1815ced5cd6e0f3c8235a7b9f137d1261ac53a80
awen <awen@163.com>
2019-08-07 15:15:41 +0800

    'Msite主题'

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import Vue from "vue"
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header/Header.vue'
 
 Vue.config.productionTip = false
-
-// new Vue({
-//   el: '#app',
-//   components: {
-//     App
-//   },
-//   template: '<App/>',
-//   router, // 配置路由器
-// }) 
+Vue.component('Header', Header)
 new Vue({
     el: '#app',
     render: h => h(App),  // 函数返回组件标签<App/>
