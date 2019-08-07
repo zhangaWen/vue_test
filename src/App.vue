3eaad20436672eef58a5cb9667ff52f80fafc82d
awen <awen@163.com>
2019-08-07 14:45:07 +0800

    '底部导航‘'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        App
+        <router-view></router-view>
+        <FooterGuide/>
     </div>
 </template>
 <script>
+    import FooterGuide from './components/FooterGuide/FooterGuide.vue'
     export default {
-        
+        components: {
+            FooterGuide
+        }
     }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 import Vue from "vue"
 import App from './App.vue'
+import router from './router'
 
+Vue.config.productionTip = false
+
+// new Vue({
+//   el: '#app',
+//   components: {
+//     App
+//   },
+//   template: '<App/>',
+//   router, // 配置路由器
+// }) 
 new Vue({
     el: '#app',
-    // comments: {
-    //     App
-    // },
-    // template: '<App/>'
-    render: h => h(App)
-})
+    render: h => h(App),  // 函数返回组件标签<App/>
+    // render: createElement => createElement(App)
+     router // 配置路由器
+  })
